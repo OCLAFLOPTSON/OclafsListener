2126a1f04a7c83178c7147303d20b2b13fde8986
Loptson <oclafloptson@gmail.com>
2025-03-04 13:50:40 -0600

    bug fix

--- oclafs_listener/listener/models.py
+++ oclafs_listener/listener/models.py
@@ -1,12 +1,12 @@
 from django.db.models import Model, DateTimeField, CharField
 
 class Log(Model):
-    date=DateTimeField()
-    player_id=CharField()
-    app_id=CharField()
-    code=CharField()
-    access_token=CharField()
-    message=CharField()
+    date=DateTimeField(max_length=500)
+    player_id=CharField(max_length=500)
+    app_id=CharField(max_length=500)
+    code=CharField(max_length=500)
+    access_token=CharField(max_length=500)
+    message=CharField(max_length=500)
 
     def __str__(self):
         return self.player_id
