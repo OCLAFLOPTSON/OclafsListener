ed456cf4ba4b582a9cda4f88f50dfc859d5c1991
Loptson <oclafloptson@gmail.com>
2025-03-04 16:04:46 -0600

    bug fix

--- oclafs_listener/listener/views.py
+++ oclafs_listener/listener/views.py
@@ -42,7 +42,7 @@ def upland_listener(request):
                 )
             Log.save()
             # Send a response to confirm receipt
-            return JsonResponse()
+            return JsonResponse(status=200)
         except JSONDecodeError:
             return JsonResponse({"status": "error", "message": "Invalid JSON"}, status=400)
     else:
